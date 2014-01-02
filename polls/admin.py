3f994d0b8eb3812648c952f4c10f0e595da8f353
Ivan Trapeznikov <trapeznikoff@yandex.ru>
2014-01-02 13:05:09 +0400

    some views added and urls signed by these views

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from polls.models import Poll,Choice
+from django.utils import timezone
+import datetime
 
-# Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    list_display = ['question', 'pub_date', 'was_published_recently']
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question']
+
+
+
+admin.site.register(Poll, PollAdmin)
